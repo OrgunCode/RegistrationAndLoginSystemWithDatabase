09c2db772b43e4bda4f9a18a4c5b12d753d36802
OrgunCode <orgun100@gmail.com>
2024-12-29 04:31:17 +0100

    Methods for the Registration GUI Buttons: "Register" and "Cancel"

--- src/Registration.java
+++ src/Registration.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Registration extends JDialog {
     private JTextField tfName;
     private JTextField tfEmail;
     private JTextField tfPhone;
-    private JTextField tfAdress;
+    private JTextField tfAddress;
     private JPasswordField pfPassword;
     private JPasswordField pfConfirmPassword;
     private JButton btnRegister;
@@ -19,7 +21,55 @@ public class Registration extends JDialog {
         setMinimumSize(new Dimension(450, 474));
         setModal(true);
         setLocationRelativeTo(parent);
+
+        //will run when pressing "Register" on Registration GUI
+        btnRegister.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                registerUser();
+            }
+        });
+
+        //will run when pressing "Cancel" on Registration GUI
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
         setVisible(true);
+    }
+
+    private void registerUser() {
+        String name = tfName.getText();
+        String email = tfEmail.getText();
+        String phone = tfPhone.getText();
+        String address = tfAddress.getText();
+        String password = String.valueOf(pfPassword.getPassword());
+        String confirmPassword = String.valueOf(pfConfirmPassword.getPassword());
+
+        //will run if a field is empty and gives a message
+        if (name.isEmpty() || email.isEmpty() || phone.isEmpty() || address.isEmpty() || password.isEmpty()) {
+            JOptionPane.showMessageDialog(this,
+                    "Please fill in every field",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        //checks if the password and confirmPassword are the same
+        if (!password.equals(confirmPassword)) {
+            JOptionPane.showMessageDialog(
+                    this,
+                    "Confirm Password does not match, please try again.",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE
+            );
+            return;
+        }
+
+
 
     }
 
