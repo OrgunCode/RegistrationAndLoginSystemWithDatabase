7d13474f9cec93b68423a596fbe9c2b3d111bbae
OrgunCode <orgun100@gmail.com>
2024-12-29 02:37:55 +0100

    Registration GUI raw finish

---
+++ src/Registration.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Registration {
+    private JTextField tfName;
+    private JTextField tfEmail;
+    private JTextField tfPhone;
+    private JTextField tfAdress;
+    private JPasswordField pfPassword;
+    private JPasswordField pfConfirmPassword;
+    private JButton btnRegister;
+    private JButton btnCancel;
+    private JPanel registerPanel;
+}
