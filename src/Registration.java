4e1d6099754ced72dd032360e35b55d3ac156ea3
OrgunCode <orgun100@gmail.com>
2024-12-29 03:06:00 +0100

    Further adjustment of Registration GUI

--- src/Registration.java
+++ src/Registration.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class Registration {
+public class Registration extends JDialog {
     private JTextField tfName;
     private JTextField tfEmail;
     private JTextField tfPhone;
@@ -10,4 +11,19 @@ public class Registration {
     private JButton btnRegister;
     private JButton btnCancel;
     private JPanel registerPanel;
+
+    public Registration(JFrame parent) {
+        super(parent);
+        setTitle("Create a new account");
+        setContentPane(registerPanel);
+        setMinimumSize(new Dimension(450, 474));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        Registration myForm = new Registration(null);
+    }
 }
