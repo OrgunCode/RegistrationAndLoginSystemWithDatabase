69f6eabcbf037035add37f8c1c6a5925d811f93f
OrgunCode <orgun100@gmail.com>
2024-12-30 02:40:39 +0100

    runs Login GUI

--- src/Login.java
+++ src/Login.java
@@ -1,9 +1,26 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class Login {
+public class Login extends JDialog {
     private JTextField tfEmail;
     private JPasswordField pfPassword;
     private JButton btnOK;
     private JButton btnCancel;
     private JPanel loginPanel;
+
+    public Login(JFrame parent) {
+        super(parent);
+        setTitle("Login");
+        setContentPane(loginPanel);
+        setMinimumSize(new Dimension(450, 474));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Login login = new Login(null);
+    }
+
 }
