15f313b1b38c05615bd4045cd08ee73ab9844c62
OrgunCode <orgun100@gmail.com>
2024-12-30 01:58:37 +0100

    Saves input from Registration GUI in database

--- src/Registration.java
+++ src/Registration.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
+
 
 public class Registration extends JDialog {
     private JTextField tfName;
@@ -21,6 +23,8 @@ public class Registration extends JDialog {
         setMinimumSize(new Dimension(450, 474));
         setModal(true);
         setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
 
         //will run when pressing "Register" on Registration GUI
         btnRegister.addActionListener(new ActionListener() {
@@ -49,7 +53,7 @@ public class Registration extends JDialog {
         String password = String.valueOf(pfPassword.getPassword());
         String confirmPassword = String.valueOf(pfConfirmPassword.getPassword());
 
-        //will run if a field is empty and gives a message
+        //checks if a field is empty
         if (name.isEmpty() || email.isEmpty() || phone.isEmpty() || address.isEmpty() || password.isEmpty()) {
             JOptionPane.showMessageDialog(this,
                     "Please fill in every field",
@@ -69,11 +73,72 @@ public class Registration extends JDialog {
             return;
         }
 
-
+        user = addUserToDatabase(name, email, phone, address, password);
+        if (user != null) {
+            dispose();
+        } else {
+            JOptionPane.showMessageDialog(
+                    this,
+                    "Failed to register new user",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        }
 
     }
 
+    private User user;
+
+    private User addUserToDatabase(String name, String email, String phone, String address, String password) {
+        User user = null;
+        final String DB_URL = "jdbc:mysql://localhost/mystore";
+        final String USERNAME = "root";
+        final String PASSWORD = "";
+
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+
+
+            Statement stmt = conn.createStatement();
+            String sql = "INSERT INTO users (name, email, phone, address, password) " +
+                    "VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, phone);
+            preparedStatement.setString(4, address);
+            preparedStatement.setString(5, password);
+
+            // Insert row into the table
+            int addedRows = preparedStatement.executeUpdate();
+            if (addedRows > 0) {
+                user = new User();
+                user.name = name;
+                user.email = email;
+                user.phone = phone;
+                user.address = address;
+                user.password = password;
+            }
+
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+
     public static void main(String[] args) {
+
         Registration myForm = new Registration(null);
+
+        User user = myForm.user;
+        if (user != null) {
+            System.out.println("Successful registration of: " + user.name);
+        } else {
+            System.out.println("Registration canceled");
+        }
     }
 }
