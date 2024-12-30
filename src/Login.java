7bd29ecd402bc714288b3c75cdfe4b7501af00b2
OrgunCode <orgun100@gmail.com>
2024-12-30 02:32:33 +0100

    Login GUI raw finish

---
+++ src/Login.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Login {
+    private JTextField tfEmail;
+    private JPasswordField pfPassword;
+    private JButton btnOK;
+    private JButton btnCancel;
+    private JPanel loginPanel;
+}
